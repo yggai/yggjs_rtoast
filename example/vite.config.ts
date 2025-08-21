b32c7eaf7cad0dbc7bd9e69859aaf62526ecb79e
yggai <pygosuperman@outlook.com>
2025-08-21 09:02:20 +0800

    打包优化

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const useLocalSrc = process.env.USE_LOCAL_SRC === '1'
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      'yggjs_rtoast': path.resolve(__dirname, '../src'),
-      'yggjs_rtoast/tech': path.resolve(__dirname, '../src/tech'),
-    }
+    alias: useLocalSrc
+      ? {
+          'yggjs_rtoast': path.resolve(__dirname, '../src'),
+          'yggjs_rtoast/tech': path.resolve(__dirname, '../src/tech'),
+        }
+      : {},
   },
   server: {
     port: 3000,
